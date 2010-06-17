e1b27c80862c8b653e46fd6cac7ce7e6f659a838
Benjamin Schuster-Böckler <ben@pearcomp.com>
2010-06-17 11:14:57 +0200

    replaced print with logging call

--- jobs/pull_report.py
+++ jobs/pull_report.py
@@ -6,6 +6,7 @@ from google.appengine.api import mail
 
 import datetime
 import sys
+import logging
 
 from lib import itcscrape
 import settings
@@ -22,17 +23,19 @@ class ReportJob(webapp.RequestHandler):
             one_day = datetime.timedelta(days=1)
             yesterday = now - one_day
             date = yesterday.strftime('%m/%d/%Y')
-        print date
+        logging.info("Getting reports for %s" % date)
         # Fetch for all available accounts
         for account_name in settings.ACCOUNTS:
             try:
                 latest_report = itcscrape.getLastDayReport(settings.ACCOUNTS[account_name]['itunesconnect_username'], settings.ACCOUNTS[account_name]['itunesconnect_password'], date)
                 report_persister.persist(latest_report['filename'], latest_report['content'])
+                logging.info("Suceeded fetching reports")
             except:
                 # Download failed (timeout or report not available yet)
                 # Send email to administrator
+                logging.warn("Failed fetching report")
                 message = mail.EmailMessage(sender=settings.SETTINGS['admin_email_address'],
-											subject='[ASM] Report job failed for account: ' + account_name)
+                                            subject='[ASM] Report job failed for account: ' + account_name)
                 message.to = settings.SETTINGS['admin_email_address']
                 message.body = 'Failed to download the iTunes Connect sales report for: ' + date
                 message.send()
